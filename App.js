e07752be5c57adc1e604b6253bcc4dfb7a192e2c
blackadderIII <skobarnes1@gmail.com>
2024-02-01 15:29:12 +0000

    started to practice

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome To New Genesis</Text>
+      <StatusBar style="dark" />
     </View>
   );
 }

5fa258f1d772d6f64fd7f88b291207cec40467bd
blackadderIII <skobarnes1@gmail.com>
2024-02-02 13:37:02 +0000

    ..

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Video } from 'expo-av';
+
+import Controls from './Controls.js';
+import AlbumCover from './AlbumCover.js';
+import AlbumDetails from './AlbulmDetails.js';
+import { TRACKS } from './tracks-data.js';
 
 export default function App() {
+  const [selectedTrack, setSelectedTrack] = useState(0);
+  const currentTrack = TRACKS[selectedTrack];
+
+  const [pause, setPause] = useState(false);
+
+  // useRef to hold the reference to the Audio element
+  const audioRef = useRef(null);
+
+  function togglePlayBtn() {
+    setPause(!pause);
+
+    if (audioRef.current) {
+      if (!pause) {
+        audioRef.current.pauseAsync();
+      } else {
+        audioRef.current.playAsync();
+      }
+    }
+  }
+  
+
   return (
     <View style={styles.container}>
-      <Text>Welcome To New Genesis</Text>
-      <StatusBar style="dark" />
+      <AlbumCover imageSource={currentTrack.albumArtUrl} />
+
+      <AlbumDetails trackName={currentTrack.title} trackArtist={currentTrack.artist} />
+
+      <Controls {...{ togglePlayBtn }} {...{ pause }} />
+
+      <Video
+        ref={audioRef}
+        source={require('./music1.mp3')}
+        shouldPlay={!pause}
+        isLooping={false}
+      />
+
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +53,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#223',
   },
 });
